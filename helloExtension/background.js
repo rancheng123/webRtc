1a643700ccef7e035c98e7521336bf97cd4b02f7
rancheng <ran.cheng@mydreamplus.com>
2020-05-11 14:18:16 +0800

    11

--- helloExtension/background.js
+++ helloExtension/background.js
@@ -27,7 +27,7 @@ chrome.runtime.onInstalled.addListener(function() {
             {
                 conditions: [
                     new chrome.declarativeContent.PageStateMatcher({
-                        pageUrl: {hostEquals: '192.168.1.6:9001'},
+                        pageUrl: {hostEquals: '10.28.12.40:9001'},
                     })
                 ],
                 actions: [new chrome.declarativeContent.ShowPageAction()]
